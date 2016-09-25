e676a8744aeccceca4c3252a2b16e40f0d8a4cc2
mikeyxcyu <michaely7@live.com>
2016-09-25 12:37:50 -0600

    housekeeping

---
+++ 1/asd.java
@@ -0,0 +1,11 @@
+public class asd{
+	public static void main(String[] args)
+	{
+		double num = 1.005e2;
+int x = 2;
+float y = (int) num * (float) x;
+System.out.print(y);
+if ('a' < 'b')
+{	System.out.println("fuasd");
+}
+}}
